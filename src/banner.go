78c8c669e82d901bafe8dcc9f9e638dedef013e3
doct3rjohn <ada7700ada@gmail.com>
2023-06-06 08:14:15 +0800

    added version v.0.1

--- src/banner.go
+++ src/banner.go
@@ -8,7 +8,7 @@ func KPBanner() string {
 	fmt.Println(" ___ ___  _______   ")
 	fmt.Println("|   Y   )|   _   |    KnownProject - CLI tool for finding subdomain.")
 	fmt.Println("|.  1  / |.  1   |    By using the dorking technique for grabbing any subdomain.")
-	fmt.Println("|.  _  \\ |.  ____|    v.N/A")
+	fmt.Println("|.  _  \\ |.  ____|    v.0.1")
 	fmt.Println("|:  |   \\|:  |     ")
 	fmt.Println("|::.| .  |::.|      ")
 	fmt.Println("|:::1\\...;:.'|    Made with love: SHAFIQAIMAN")
